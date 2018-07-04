3b0a2284c5a451b9330e26a79becd7a6440c9f11
jalam453 <jalam453@gmail.com>
2018-07-04 15:21:15 +0530

    Year Master Created validation pending

--- masters/urls.py
+++ masters/urls.py
@@ -5,8 +5,17 @@ from . import views
 app_name = 'masters'
 
 urlpatterns = [
+
+    # ******* Year Master ************
     path('template_year_master/', views.template_year_master, name='template_year_master'),
+    path('save_year/', views.save_year, name='save_year'),
+    path('update_year/', views.update_year, name='update_year'),
+    path('delete_year/', views.delete_year, name='delete_year'),
+
+    path('template_scholarship_master/', views.template_scholarship_master, name='template_scholarship_master'),
+    path('template_degree_formula_master/', views.template_degree_formula_master,
+         name='template_degree_formula_master'),
 
     path('get_table_data/', views.get_table_data, name='get_table_data'),
-    path('save_year/', views.save_year, name='save_year'),
+
 ]
--- masters/views.py
+++ masters/views.py
@@ -1,23 +1,73 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse, HttpResponseBadRequest
 import json
+from masters.models import *
+from django.shortcuts import render, redirect, render_to_response
+from django.contrib import messages
 
-# Create your views here.
+
 def template_year_master(request):
-    data =  [{'id': 1, 'first_name': 'abc', 'last_name': 'ABCD','phone_number':9898989898},
-            {'id': 2, 'first_name': 'xyz', 'last_name': 'ABCD','phone_number':8888888888},
-            {'id': 3, 'first_name': 'pqrs', 'last_name': 'ABCD','phone_number':7777777777},
-            {'id': 4, 'first_name': 'lmnop', 'last_name': 'ABCD','phone_number':6666666666}]
-    return render(request, 'template_year_master.html',{'data':data, 'data_length':len(data)})
+    year_recs = YearDetails.objects.all()
+    return render(request, 'template_year_master.html', {'year_recs': year_recs})
+
+
+def template_scholarship_master(request):
+    data = [{'id': 1, 'first_name': 'abc', 'last_name': 'Yes', 'phone_number': 9898989898},
+            {'id': 2, 'first_name': 'xyz', 'last_name': 'No', 'phone_number': 8888888888},
+            {'id': 3, 'first_name': 'pqrs', 'last_name': 'No', 'phone_number': 7777777777},
+            {'id': 4, 'first_name': 'lmnop', 'last_name': 'No', 'phone_number': 6666666666}]
+    return render(request, 'template_scholarship_master.html', {'data': data, 'data_length': len(data)})
+
+
+def template_degree_formula_master(request):
+    data = [{'id': 1, 'first_name': 'abc', 'last_name': 'Yes', 'phone_number': 9898989898},
+            {'id': 2, 'first_name': 'xyz', 'last_name': 'No', 'phone_number': 8888888888},
+            {'id': 3, 'first_name': 'pqrs', 'last_name': 'No', 'phone_number': 7777777777},
+            {'id': 4, 'first_name': 'lmnop', 'last_name': 'No', 'phone_number': 6666666666}]
+    return render(request, 'template_degree_formula_master.html', {'data': data, 'data_length': len(data)})
 
 
 def get_table_data(request):
-
-
     # data = {'id': 1, 'column_name': 'abc', 'value': 'ABCD'}
 
     return HttpResponse(json.dumps(data), content_type="application/json")
 
+
 def save_year(request):
-    print(request.POST)
-    pass
+    year_name = request.POST.get('year_name')
+    start_date = request.POST.get('start_date')
+    end_date = request.POST.get('end_date')
+    try:
+        YearDetails.objects.create(year_name=year_name.lower(), start_date=start_date, end_date=end_date)
+        messages.success(request, "Record saved.")
+    except:
+        messages.warning(request, "Record not saved.")
+    return redirect('/masters/template_year_master/')
+
+
+def update_year(request):
+    year_id = request.POST.get('year_id')
+    year_name = request.POST.get('year_name')
+    start_date = request.POST.get('start_date')
+    end_date = request.POST.get('end_date')
+    try:
+        YearDetails.objects.filter(id=year_id).update(year_name=year_name.lower(),start_date=start_date,end_date=end_date)
+        messages.success(request, "Record updated.")
+        return HttpResponse(json.dumps({'success': 'Record updated.'}),content_type="application/json")
+
+        # messages.success(request, "Record updated.")
+    except:
+        messages.warning(request, "Record not updated.")
+    return HttpResponse(json.dumps({'error': 'Record not updated.'}), content_type="application/json")
+
+
+def delete_year(request):
+    year_id = request.POST.get('year_id')
+
+    try:
+        YearDetails.objects.filter(id=year_id).delete()
+        messages.success(request, "Record deleted.")
+        return HttpResponse(json.dumps({'success': 'Record deleted.'}),content_type="application/json")
+    except:
+        messages.warning(request, "Record not deleted.")
+    return HttpResponse(json.dumps({'error': 'Record not deleted.'}), content_type="application/json")
