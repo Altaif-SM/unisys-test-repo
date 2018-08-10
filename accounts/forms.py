b6ef549ae7eace5de7b39f9244eb172a3de042b2
abhilash2b <abhilash2b@gitlab.com>
2018-08-10 16:07:44 +0530

    bread cumbs

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate
 
 
 class loginForm(forms.Form):
-    username = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-control form-control-solid placeholder-no-fix' , 'placeholder':'Username','autocomplete': 'off'}))
+    username = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-control form-control-solid placeholder-no-fix' , 'placeholder':'Email','autocomplete': 'off'}))
     password = forms.CharField(required=True,widget=forms.PasswordInput(attrs={'class':'form-control form-control-solid placeholder-no-fix' , 'placeholder':'Password','autocomplete': 'off'}))
 
     def login(self, request):
