961d91a0593e783f846d0f4fa8a12dbb063a9a79
abhilash2b <abhilash2b@gitlab.com>
2018-11-23 15:45:09 +0530

    religions script added

--- common/init_data.py
+++ common/init_data.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 import os
-from masters.models import AllCountries
+from masters.models import AllCountries,ReligionDetails
 from student.models import PassingYear
 from datetime import datetime
 import json
 import pycountry
+
 
 """
 >>> from common.init_data import load_initial_data
@@ -22,6 +23,14 @@ def import_world_all_countries():
         AllCountries.objects.create(country_name=country.name)
 
 
+def import_relgions():
+    relgion_list = ['Jainism','Buddhism','Hinduism','Christianity','Gnosticism','Islam','Judaism','Sikhism']
+    for rec in relgion_list:
+        print(rec)
+        ReligionDetails.objects.create(religion_name=rec)
+
+
 def load_initial_data():
     import_years()
     import_world_all_countries()
+    import_relgions()
