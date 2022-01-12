02338f7dd725cf940e799206a10dd1e7c0308732
riyaz <riyaz_redbytes>
2022-01-12 12:51:21 +0530

    country ordering updated!

--- masters/models.py
+++ masters/models.py
@@ -26,7 +26,7 @@ class CountryDetails(BaseModel):
     country_name = models.CharField(max_length=255, blank=True, null=True)
 
     class Meta:
-        ordering = ('country_name',)
+        ordering = ('id',)
 
     def __str__(self):
         return self.country_name
