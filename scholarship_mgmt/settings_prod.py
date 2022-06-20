83a8cb08544dff690e60974bc0b31b94fc9bcaf2
riyaz <riyaz_redbytes>
2022-06-20 14:29:42 +0530

    email and password updated!

--- scholarship_mgmt/settings_prod.py
+++ scholarship_mgmt/settings_prod.py
@@ -190,8 +190,8 @@ STATICFILES_DIRS = (
 MAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sayyedriyaj55@gmail.com'
-EMAIL_HOST_PASSWORD = 'Sayyed@123'
+EMAIL_HOST_USER = 'riyaz@altaif.com.sg'
+EMAIL_HOST_PASSWORD = 'newm<GnE2ub'
 EMAIL_USE_TLS = True
 
 
